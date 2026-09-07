86b54d6671772d724c4aa694eff6f370991c8906
Jeremiah Butler <git@jbutler.dev>
2026-09-07 16:38:31 -0400

    Footer: link to lab.jbutler.dev (The Lab)

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -20,6 +20,7 @@ class Footer extends Component {
 
            <ul className="copyright">
               <li>&copy; Jeremiah Butler</li>
+              <li><a title="Live dashboard of the home lab" href="https://lab.jbutler.dev" target="_blank" rel="noreferrer">The Lab &mdash; lab.jbutler.dev</a></li>
 	      <li> Inspired by <a href="https://github.com/tbakerx/react-resume-template">Tim Baker</a></li>
               <li>Design by <a title="Styleshout" href="http://www.styleshout.com/">Styleshout</a></li>
            </ul>
